0330d428cd111724c6d994782d0306b92a6e806e
Genesis Sarabia <genesissarabia14@gmail.com>
2023-08-31 00:02:24 -0500

    cleaned up code even more, took out props altogether from parameter

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,12 +1,14 @@
 
-function ProfileCard(props){
+function ProfileCard({title, handle}){
     // const title = props.title;
     // const handle = props.handle;
-    const {title, handle} = props;
-    return <div>
+    // const {title, handle} = props;
+    return (
+    <div>
         <div>Title is {title}</div>
         <div>Handle is {handle}</div>
     </div>
+    );
 }
 
 export default ProfileCard;
