bbae16a5995396d3ebda0adc476ade18e42fec2c
Genesis Sarabia <genesissarabia14@gmail.com>
2023-08-30 23:58:46 -0500

    used more efficient shorter js code in jsx for profCard comp to lessen code

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,8 +1,11 @@
 
 function ProfileCard(props){
+    // const title = props.title;
+    // const handle = props.handle;
+    const {title, handle} = props;
     return <div>
-        <div>Title is {props.title}</div>
-        <div>Handle is {props.handle}</div>
+        <div>Title is {title}</div>
+        <div>Handle is {handle}</div>
     </div>
 }
 
