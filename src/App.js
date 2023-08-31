354c7c754f6a17840102f23e5be9e526b9e5a0e9
Genesis Sarabia <genesissarabia14@gmail.com>
2023-08-30 23:51:44 -0500

    added props for rest of profcard components

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App(){
             <div>Personal Digital Assistants</div>
 
             <ProfileCard title= "Alexa" handle="@alexa99"/>
-            <ProfileCard />
-            <ProfileCard />
+            <ProfileCard title="Cortana" handle="@cortana32"/>
+            <ProfileCard title="Siri" handle="@siri01"/>
         </div>
     )
 }
