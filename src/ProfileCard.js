8d0296775f6ddcb8d7ee780f997a71379c81d73b
Genesis Sarabia <genesissarabia14@gmail.com>
2023-08-31 00:02:40 -0500

    added comment to explained how we further cleaned

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -3,6 +3,9 @@ function ProfileCard({title, handle}){
     // const title = props.title;
     // const handle = props.handle;
     // const {title, handle} = props;
+    // ****took out props altogether from parameter and since were only
+    // focusing on title and handle props we can just stick it as an object
+    // directly in parameter for function****
     return (
     <div>
         <div>Title is {title}</div>
