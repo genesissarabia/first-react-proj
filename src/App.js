803802361d4d9a4de2d568659cb84e9fe940ffe0
Genesis Sarabia <genesissarabia14@gmail.com>
2023-09-01 20:03:15 -0500

    imported images officially and learned syntax

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import AlexaImage from "./images/alexa.png"
 import CortanaImage from "./images/cortana.png"
 import SiriImage from "./images/siri.png"
 
+
 function App(){
     return (
         <div>
             <div>Personal Digital Assistants</div>
 
+            <img src={AlexaImage}/>
+            <img src={CortanaImage}/>
+            <img src={SiriImage}/>
             <ProfileCard title= "Alexa" handle="@alexa99"/>
             <ProfileCard title="Cortana" handle="@cortana32"/>
             <ProfileCard title="Siri" handle="@siri01"/>
