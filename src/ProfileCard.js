280b55a01adc79f52a78ac59e8d9989295bfbb71
Genesis Sarabia <genesissarabia14@gmail.com>
2023-09-06 19:55:41 -0500

    completed basic project

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,4 +1,4 @@
-function ProfileCard({title, handle, image}) {
+function ProfileCard({title, handle, image, description}) {
     // const title = props.title;
     // const handle = props.handle;
     // const {title, handle} = props;
@@ -16,6 +16,9 @@ function ProfileCard({title, handle, image}) {
                 <div className="media-content">
                     <p className="title is-4">{title}</p>
                     <p className="subtitle is-6">{handle}</p>
+                </div>
+                <div className="content">
+                    {description}
                 </div>
             </div>
         </div>
