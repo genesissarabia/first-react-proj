65b8f8234dfeed80755b9bf5f31938e5949bb43b
Genesis Sarabia <genesissarabia14@gmail.com>
2023-09-06 19:49:32 -0500

    styling header title

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import CortanaImage from "./images/cortana.png"
 import SiriImage from "./images/siri.png"
 
 
-function App(){
+function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
-
+            <section className="hero is-primary">
+                <div className='hero-body'>
+                    <p className='title'>Personal Digital Assistants</p>
+                </div>
+            </section>
             {/*<img src={AlexaImage}/>*/}
             {/*<img src={CortanaImage}/>*/}
             {/*<img src={SiriImage}/>*/}
@@ -17,13 +20,16 @@ function App(){
                 <section className="section">
                     <div className="columns">
                         <div className="column is-4">
-                            <ProfileCard title= "Alexa" handle="@alexa99" image={AlexaImage}/>
+                            <ProfileCard title="Alexa" handle="@alexa99"
+                                         image={AlexaImage}/>
                         </div>
                         <div className="column is-4">
-                            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImage}/>
+                            <ProfileCard title="Cortana" handle="@cortana32"
+                                         image={CortanaImage}/>
                         </div>
                         <div className="column is-4">
-                            <ProfileCard title="Siri" handle="@siri01" image={SiriImage}/>
+                            <ProfileCard title="Siri" handle="@siri01"
+                                         image={SiriImage}/>
                         </div>
                     </div>
                 </section>
