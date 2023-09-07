e6ddab8ab6eda7e8b61dd5ba664206616c82c3c0
Genesis Sarabia <genesissarabia14@gmail.com>
2023-09-06 19:54:08 -0500

    adding descriptions to my cards

--- src/App.js
+++ src/App.js
@@ -18,16 +18,25 @@ function App() {
                 <section className="section">
                     <div className="columns">
                         <div className="column is-4">
-                            <ProfileCard title="Alexa" handle="@alexa99"
-                                         image={AlexaImage}/>
+                            <ProfileCard title="Alexa"
+                                         handle="@alexa99"
+                                         image={AlexaImage}
+                                         description="Alexa was created by Amazon and helps you buy things."
+                            />
                         </div>
                         <div className="column is-4">
-                            <ProfileCard title="Cortana" handle="@cortana32"
-                                         image={CortanaImage}/>
+                            <ProfileCard title="Cortana"
+                                         handle="@cortana32"
+                                         image={CortanaImage}
+                                         description="Cortana was created by Microsoft. Never used it."
+                            />
                         </div>
                         <div className="column is-4">
-                            <ProfileCard title="Siri" handle="@siri01"
-                                         image={SiriImage}/>
+                            <ProfileCard title="Siri"
+                                         handle="@siri01"
+                                         image={SiriImage}
+                                         description="Siri was created by Apple and you can adapt her to different accents."
+                            />
                         </div>
                     </div>
                 </section>
